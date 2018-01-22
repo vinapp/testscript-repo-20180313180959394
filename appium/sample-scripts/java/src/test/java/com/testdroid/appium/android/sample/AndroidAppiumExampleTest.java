3c985270a31904f2dcedb2d63641c59875c9558c
Vinod Appajanna <vinapp@users.noreply.github.com>
2018-01-22 16:45:30 +0530

    Update AndroidAppiumExampleTest.java

--- appium/sample-scripts/java/src/test/java/com/testdroid/appium/android/sample/AndroidAppiumExampleTest.java
+++ appium/sample-scripts/java/src/test/java/com/testdroid/appium/android/sample/AndroidAppiumExampleTest.java
@@ -76,7 +76,7 @@ public class AndroidAppiumExampleTest  extends BaseAndroidTest {
         contextField.sendKeys(serverutil.getMobContext());
 
         //takeScreenshot("Login details entered");
-        ((AndroidDriver)wd).sendKeyEvent(AndroidKeyCode.ENTER);
+        wd.sendKeyEvent(AndroidKeyCode.ENTER);
 
         try {
             Thread.sleep(5000);
