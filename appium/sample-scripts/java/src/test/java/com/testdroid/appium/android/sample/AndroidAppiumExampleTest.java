f5c88e66efd620a98374ee1e79fb0a6cf72ec679
Vinod Appajanna <vinapp@users.noreply.github.com>
2018-01-22 14:45:30 +0530

    Update AndroidAppiumExampleTest.java

--- appium/sample-scripts/java/src/test/java/com/testdroid/appium/android/sample/AndroidAppiumExampleTest.java
+++ appium/sample-scripts/java/src/test/java/com/testdroid/appium/android/sample/AndroidAppiumExampleTest.java
@@ -26,13 +26,47 @@ public class AndroidAppiumExampleTest  extends BaseAndroidTest {
     @Test
     public void mainPageTest() throws IOException, InterruptedException {
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-        takeScreenshot("start");
-        wd.findElement(By.xpath("//android.widget.RadioButton[@text='Use Testdroid Cloud']")).click();
-        wd.findElement(By.xpath("//android.widget.EditText[@resource-id='com.bitbar.testdroid:id/editText1']")).sendKeys("John Doe");
-        takeScreenshot("after_adding_name");
-        wd.navigate().back();
-        wd.findElement(By.xpath("//android.widget.ScrollView[1]/android.widget.LinearLayout[1]/android.widget.LinearLayout[2]/android.widget.Button[1]")).click();
-        takeScreenshot("after_answer");
-    }
+        
+        //*********Enter the login details and login*********//
+        WebElement userField = driver.findElement(By.id("appcenter_Login_login"));
+        userField.click();
+        userField.sendKeys(serverutil.getUsername());
 
+        WebElement passwordField = driver.findElement(By.id("appcenter_Login_pw"));
+        passwordField.click();
+        passwordField.sendKeys(serverutil.getPassword());
+
+        WebElement hostField = driver.findElement(By.id("appcenter_Login_server"));
+        hostField.click();
+        hostField.sendKeys(serverutil.getHost());
+
+        WebElement portField = driver.findElement(By.id("appcenter_Login_port"));
+        portField.click();
+        portField.sendKeys(""+ serverutil.getPort() + "");
+
+        WebElement contextField = driver.findElement(By.id("appcenter_Login_ctx"));
+        contextField.click();
+        contextField.sendKeys(serverutil.getMobContext());
+
+        //takeScreenshot("Login details entered");
+        ((AndroidDriver)driver).sendKeyEvent(AndroidKeyCode.ENTER);
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        //*********Catalog: Displaying the apps*********//
+        WebElement catalogElement = wait.until(
+                ExpectedConditions.presenceOfElementLocated(By.xpath("//android.view.View[@content-desc='Catalog']")));
+
+        //takeScreenshot("Catalog Screen");
+        if (catalogElement.isDisplayed()) {
+            AssertJUnit.assertEquals(1, 1);
+        } else {
+            AssertJUnit.assertEquals(1, 0);//fail the case
+        }
+    }
 }
